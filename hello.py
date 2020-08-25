586aa12c7ab0a5ff63f718c5c4797fe3f74f1815
yara007 <yarafortim80@gmail.com>
2020-08-25 13:38:14 +0000

    cria o arquivo olá

---
+++ hello.py
@@ -0,0 +1,38 @@
+  # exemplo  comando print
+print('hello')
+
+  # exemplo variaveis
+nome = "Patricia"
+idade = 25
+altura = 1.70
+peso = 78.50
+status = False
+status = True
+
+  # exemplo lista
+lista = ['pão','manteiga','café','leite','queijo']
+
+  # exemplo  comando print
+print('A vida exige muito de nós, mas quem tem coragem de arriscar pode conquistar o mundo')
+  # exemplo lista e comando print
+pessoa = [ 'Maria Clara', 30, 1.65, 'Medica']
+print(f'nome: {pessoa[0]}' )
+print(f'idade: {pessoa[1]}' )
+print(f'altura: {pessoa[2]}' )
+print(f'profissão: {pessoa[3]}')
+
+
+# exemplo dicionario
+
+pessoa = {
+    'nome': ' Juliana silva ',
+    'idade': 35,
+    'endereço':  'rua Ana Mota,fortim-ce ',
+    'cep': 62815-000,
+    'cpf_no_serasa': False,
+    'telefone': ['(88) 9973-4562', '(85) 9962-2345']
+}
+
+print(pessoa)
+
+
